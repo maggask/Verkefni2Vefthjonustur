42adf4c0809c40d2e772b17e32ae036029e502df
Maggnez <maggask@gmail.com>
2014-09-29 11:01:28 +0000

    Added the needed tables, entities for them to map between objects, the controllers we need and the serviceproviders that we need

---
+++ CoursesAPI.Services/Models/Entities/Grade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Services.Models.Entities
+{
+    class Grade
+    {
+        /// <summary>
+        /// A database-generated ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// A reference to the Projects table.
+        /// </summary>
+        public int ProjectID { get; set; }
+
+        /// <summary>
+        /// A students grade.
+        /// </summary>
+        public float Grade { get; set; }
+
+        /// <summary>
+        /// A reference to the Person table, SSN.
+        /// </summary>
+        public String PersonID { get; set; }
+    }
+}
---
+++ CoursesAPI.Services/Models/Entities/Project.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Services.Models.Entities
+{
+    class Project
+    {
+        /// <summary>
+        /// A database-generate ID.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name of the project, e.g skilaverkefni1, lokapróf,
+        /// netpróf1, miðannarpróf.
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// A reference to the CourseInstance table. The
+        /// instance of a course that e.g the project
+        /// applies to.
+        /// </summary>
+        public int CourseInstanceID { get; set; }
+
+        /// <summary>
+        /// Some parts like midtermtest (miðannarpróf) 
+        /// can help push up the final exam (lokapróf) 
+        /// grade if a student scores higher on the 
+        /// midtermtest than the final exam. ?????
+        /// </summary>
+        public int OnlyHigherThanProjectID { get; set; }
+
+        /// <summary>
+        /// The weigth of a project, test et cetera, 5 is 5%,
+        /// 10 is 10% and so on.
+        /// </summary>
+        public int Weight { get; set; }
+
+        /// <summary>
+        /// The minimal grade to be able to pass
+        /// the course. That is e.g final grade
+        /// of the final exam and the final grade
+        /// in the course.
+        /// </summary>
+        public float MinGradeToPassCourse { get; set; }
+    }
+}
