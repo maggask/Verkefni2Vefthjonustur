e1d3b156fab73e95aa5f5360ac7306d9c2542d11
Einar Thor <einarthor13@ru.is>
2014-09-29 15:44:34 +0000

    View model and stuff

---
+++ CoursesAPI.Models/ProjectCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Models
+{
+    public class ProjectCreateViewModel
+    {
+        public String   Name                     { get; set; }
+        public int      Weight                   { get; set; }
+        public float    MinGradeToPassCourse     { get; set; }
+        public int      CourseInstanceID         { get; set; }
+        public int      OnlyHigherThanProjectID  { get; set; }
+    }
+}
