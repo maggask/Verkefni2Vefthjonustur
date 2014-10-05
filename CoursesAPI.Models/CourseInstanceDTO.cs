f3641ba9761233b08e98967776c8d38385d2d548
Maggnez <maggask@gmail.com>
2014-10-05 20:14:16 +0000

    Just added comments to a DTO class

--- CoursesAPI.Models/CourseInstanceDTO.cs
+++ CoursesAPI.Models/CourseInstanceDTO.cs
@@ -1,15 +1,34 @@
 ﻿namespace CoursesAPI.Models
 {
 	/// <summary>
-	/// 
+	/// DTO class for an instance of a course.
 	/// </summary>
 	public class CourseInstanceDTO
 	{
+        /// <summary>
+        /// Auto-generated id for a course instance.
+        /// </summary>
 		public int    CourseInstanceID { get; set; }
+
+        /// <summary>
+        /// ID of a course, foreign key.
+        /// </summary>
 		public string CourseID         { get; set; }
+
+        /// <summary>
+        /// Name of the course.
+        /// </summary>
 		public string Name             { get; set; }
+
+        /// <summary>
+        /// The general description of the course.
+        /// </summary>
         public string Description      { get; set; }
 
+        /// <summary>
+        /// The main teacher of the course,
+        /// if it has one.
+        /// </summary>
 		public string MainTeacher      { get; set; }
 	}
 }
